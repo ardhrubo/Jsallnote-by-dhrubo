c6f2b3209bc46c9ac32d1597a960aedbd10ae673
ardhrubo <ardhrubo3038@gmail.com>
2023-12-18 17:22:35 +0600

    added

--- chapter_eight/argument.js
+++ chapter_eight/argument.js
@@ -1,62 +1,62 @@
 
-// function add(a,b) {
-//     var result = a+b
-//     console.log(result)
+function add(a,b) {
+    var result = a+b
+    console.log(result)
 
+}
+
+add(10,20)
+add (20,40)
+add()
+
+
+
+function sub(c,d)
+{
+    var result2= c-d
+    console.log(result2)
+}
+sub(3,4)
+
+
+var arr1 = [1,2,3]
+var arr2 = [5,6,7]
+var arr3 = [9,7,1]
+
+// sum1=0
+
+// for (var i=0; i<arr1.length; i++) {
+//     sum1+= arr1[i]
 // }
 
-// add(10,20)
-// add (20,40)
-// add()
+// console.log(sum1)
 
-
-
-// function sub(c,d)
-// {
-//     var result2= c-d
-//     console.log(result2)
-// }
-// sub(3,4)
-
-
-// var arr1 = [1,2,3]
-// var arr2 = [5,6,7]
-// var arr3 = [9,7,1]
-
-// // sum1=0
-
-// // for (var i=0; i<arr1.length; i++) {
-// //     sum1+= arr1[i]
-// // }
-
-// // console.log(sum1)
-
-// // sum2=0
-// // for (var i=0; i<arr2.length; i++) {
-// //     sum2+= arr2[i]
-// // }
-
-// // console.log(sum2)
-
-// // sum3=0
-// // for (var i=0; i<arr3.length; i++) {
-// //     sum3+= arr3[i]
-// // }
-
-// // console.log(sum3)
-
-
-// function sumofarray(arr) {
-//     sum=0
-
-//     for (var i=0; i<arr.length; i++) {
-//     sum+= arr[i]
-// }
-//  console.log(sum)
+// sum2=0
+// for (var i=0; i<arr2.length; i++) {
+//     sum2+= arr2[i]
 // }
 
+// console.log(sum2)
 
-// sumofarray(arr3)
+// sum3=0
+// for (var i=0; i<arr3.length; i++) {
+//     sum3+= arr3[i]
+// }
+
+// console.log(sum3)
+
+
+function sumofarray(arr) {
+    sum=0
+
+    for (var i=0; i<arr.length; i++) {
+    sum+= arr[i]
+}
+ console.log(sum)
+}
+
+
+sumofarray(arr3)
 
 
 function test () {
