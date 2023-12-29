eb149b4460d23a17680a53f4304720f2db9ab64e
ardhrubo <ardhrubo3038@gmail.com>
2023-12-29 11:19:39 +0600

    added

--- module/index.js
+++ module/index.js
@@ -3,6 +3,12 @@ import Rectangle from "./Rectangle.js"
 
 import * as func from './func.js';
 
+// import {add, div} from './func.js'
+
+// console.log(add(23,33))
+// console.log(div(33,3))
+
+
 console.log(func.add(10,2))
 console.log(func.times(10,2))
 console.log(func.sub(10,2))
---
+++ module/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+console.log(func.add(10,2))
+console.log(func.times(10,2))
+console.log(func.sub(10,2))
+console.log(func.div(10,2))
