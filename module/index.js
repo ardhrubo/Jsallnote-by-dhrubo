6a86292ac8f5f062a7d9a66537d1319b125ec424
ardhrubo <ardhrubo3038@gmail.com>
2023-12-29 11:15:31 +0600

    all jog

--- module/index.js
+++ module/index.js
@@ -4,7 +4,9 @@ import Rectangle from "./Rectangle.js"
 import * as func from './func.js';
 
 console.log(func.add(10,2))
-
+console.log(func.times(10,2))
+console.log(func.sub(10,2))
+console.log(func.div(10,2))
 let r = new Rectangle('blue',4,5)
 
 
