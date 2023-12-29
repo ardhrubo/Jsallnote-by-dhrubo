62054883e53f8ccdf04dddad5199cdccaa167faf
ardhrubo <ardhrubo3038@gmail.com>
2023-12-29 20:45:23 +0600

    handle range error

--- chapter_sixteen/index.js
+++ chapter_sixteen/index.js
@@ -1,11 +1,26 @@
 
-let n = 10
+let n = 1
 if (n>5){
-    throw new Error('N is greater than 10')
+    // throw new Error('N is greater than 10')
+}
+
+if (n>5){
+    throw new RangeError('Provide a small num');
+    
 }
 
 
 
 
+function changeToInt(v){
+    let result = Number.parseInt(v)
+    if(!result){
+        return 'Please provide a value'
+    }
+    return result
+}
 
 
+let result = changeToInt(5)
+
+console.log(result)
