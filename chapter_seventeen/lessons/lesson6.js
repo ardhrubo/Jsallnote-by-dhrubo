f43d780d46f871b8ae6428c1dac077a70c637c5a
ardhrubo <ardhrubo3038@gmail.com>
2024-01-01 23:42:08 +0600

    added

--- chapter_seventeen/lessons/lesson6.js
+++ chapter_seventeen/lessons/lesson6.js
@@ -1,14 +1,39 @@
 function getRequest(url,callback){
-    const xhr = XMLHttpRequest()
+    const xhr =  new XMLHttpRequest()
     xhr.open('get',url)
 
     xhr.onreadystatechange = function(e){
-        if(xhr.readyState === 4){
-            if(xhr.status){
-                
+        if (xhr.readyState===4){
+            if(xhr.status ===200){
+                let response = JSON.parse(xhr.response)
+                callback(null,response)   
+            } else{
+                callback(xhr.response,null)
             }
         }
     }
+    xhr.send()
 
 }
+
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+getRequest(`${BASE_URL}/posts/1`,(err,res) => {
+ 
+    if(err){
+        throw new Error('Error occured')
+    } 
+    let { userID} = res
+    getRequest(`${BASE_URL}/users/${userID}`,(err,res) =>{
+        if(err){
+            throw new Error('Error Occured')
+        }
+        getRequest(`${BASE_URL}/posts/1/comments`)
+    })
+    
+})
+
+
+
 
