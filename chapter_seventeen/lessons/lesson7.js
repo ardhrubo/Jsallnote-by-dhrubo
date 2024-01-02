6480bf23d5b72f5c8bf0bd55bbf008f054d859fe
ardhrubo <ardhrubo3038@gmail.com>
2024-01-02 20:38:11 +0600

    implement fetch api

--- chapter_seventeen/lessons/lesson7.js
+++ chapter_seventeen/lessons/lesson7.js
@@ -1,23 +1,54 @@
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
-fetch(`${BASE_URL}/users/1`)
-.then(res => res.json())
+// fetch(`${BASE_URL}/users/1`)
+// .then(res => res.json())
+//     .then(data=>{
+//         console.log(data)
+//         return Promise.resolve('something')
+//     }) 
+//     .then(str => {
+//         console.log(str)
+//         return Promise.resolve('Another promise')
+//     })
+//     .then(another=>{
+//         console.log(another)
+//     })
+
+// .catch((e)=>{
+//     console.log(e)
+// })
+
+
+
+
+function getRequest(url){
+   return new Promise((resolve,reject)=>{
+    const xhr =  new XMLHttpRequest()
+    xhr.open('get',url)
+    
+    xhr.onreadystatechange = function(e){
+        if (xhr.readyState===4){
+            if(xhr.status ===200){
+                let response = JSON.parse(xhr.response)
+                    resolve(response)
+            } else{
+                reject(new Error('Error occured'))
+            }
+        }
+    }
+    xhr.send()
+
+    }) 
+}
+
+
+let res = getRequest(`${BASE_URL}/usew55346312ers/1`)
     .then(data=>{
         console.log(data)
-        return Promise.resolve('something')
-    }) 
-    .then(str => {
-        console.log(str)
-        return Promise.resolve('Another promise')
     })
-    .then(another=>{
-        console.log(another)
+    .catch( e=>{
+        console.log(e)
     })
-
-.catch((e)=>{
-    console.log(e)
-})
-
 
 
