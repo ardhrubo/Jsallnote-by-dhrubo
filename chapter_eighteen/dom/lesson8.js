2daec27b29befbeb099dd45317436690381cb276
ardhrubo <ardhrubo3038@gmail.com>
2024-01-07 20:48:53 +0600

    dom

--- chapter_eighteen/dom/lesson8.js
+++ chapter_eighteen/dom/lesson8.js
@@ -1,12 +1,45 @@
-let li = document.createElement('li')
+let li = createElement('li','four','list-group-item')
 
-li.className = 'list-group-item'
-li.setAttribute('title','I am fourth Item')
-li.innerHTML = 'four'
+
 
 let list = document.getElementById('list')
 
 list.appendChild(li)
+
+
+
+
+let p1 = createElement('p','Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam nesciunt, magnam tempore veritatis pariatur. Deserunt praesentium ullam iure vero','lead')
+
+let p2 = createElement('p','Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam nesciunt, magnam tempore veritatis pariatur. Deserunt praesentium ullam iure vero','lead')
+
+let div = createElement('div')
+append(div,[p1,p2])
+
+list.insertAdjacentElement('beforebegin',div)
+
+
+// let container = document.getElementById('container')
+
+// container.appendChild(div)
+
+
+function createElement(tagName,innerHTML,className){
+
+    let tag = document.createElement(tagName)
+    tag.innerHTML = innerHTML || ''
+    tag.className = className || ''
+    return tag
+    
+    }
+    
+
+function append(parent,children){
+
+    children.forEach(child=> parent.appendChild(child))
+
+}
+
 
 
 
