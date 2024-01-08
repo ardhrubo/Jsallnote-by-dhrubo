956408944b8bda8493264682562893139af0001a
ardhrubo <ardhrubo3038@gmail.com>
2024-01-08 14:57:11 +0600

    added or removing new item

--- chapter_eighteen/dom/lesson10.js
+++ chapter_eighteen/dom/lesson10.js
@@ -7,10 +7,35 @@ let btn = document.getElementById('btn')
 let list = document.getElementById('list')
 
 btn.addEventListener('click',function(e){
-    let item = list.lastElementChild.cloneNode(true)
-    item.innerHTML = 'Newly created item'
-    list.appendChild(item)
+    let item = list.lastElementChild
+    let newItem ;
+    if(item){
+        newItem = item.cloneNode(true)
+    }else{
+        newItem = document.createElement('li')
+    }
+    newItem.innerHTML = 'Newly created item'
+    list.appendChild(newItem)
+
+});
+
+
+
+list.addEventListener('click',function(e){
+     if(this.contains(e.target)){
+        e.target.remove()
+     }
 })
+
+
+
+// Event Delegation Problem
+// [...list.children].forEach(li=>{
+//     li.onclick = function(e){
+//         e.target.remove(e)
+//     }
+// })
+
 
 
 let box = document.getElementById('box')
