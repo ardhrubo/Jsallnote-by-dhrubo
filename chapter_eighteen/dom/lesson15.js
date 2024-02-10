ea90eb96cde0579105c6f5259c2e1c56f6951441
ardhrubo <ardhrubo3038@gmail.com>
2024-02-11 00:13:13 +0600

    fetch

--- chapter_eighteen/dom/lesson15.js
+++ chapter_eighteen/dom/lesson15.js
@@ -1,34 +1,76 @@
 
-let load = document.getElementById('load')
-let postList = document.getElementById('posts')
+// let load = document.getElementById('load')
+// let postList = document.getElementById('posts')
 
-const URL = 'https://jsonplaceholder.typicode.com/posts'
+// const URL = 'https://jsonplaceholder.typicode.com/posts'
 
-load.addEventListener('click',function(){
-    fetch(URL)
-            .then(response => response.json())
-            .then(posts=>{
+// load.addEventListener('click',function(){
+//     fetch(URL)
+//             .then(response => response.json())
+//             .then(posts=>{
                 
 
-                // posts.forEach((post,index) => {
-                //     let listitem = listItemGenerator(post,index+1)
-                //     postList.appendChild(listitem)
-                // })
+//                 // posts.forEach((post,index) => {
+//                 //     let listitem = listItemGenerator(post,index+1)
+//                 //     postList.appendChild(listitem)
+//                 // })
 
 
-                // let postsList = posts.map((post,index)=> 
-                // listItemGenerator(post,index+1))
+//                 // let postsList = posts.map((post,index)=> 
+//                 // listItemGenerator(post,index+1))
 
-                // postList.appendChild(...postsList)
+//                 // postList.appendChild(...postsList)
 
-            })
-            .catch(e => console.log(e.message))
+//             })
+//             .catch(e => console.log(e.message))
+// })
+
+
+// function listItemGenerator(item,no){
+//     let li = document.createElement('li')
+//     li.className = 'list-group-item'
+//     li.innerHTML = `${no}. ${item.title}`
+//     return li
+// }
+
+
+let postForm = document.getElementById('post-form')
+let posts = document.getElementById('posts')
+const URL = 'https://jsonplaceholder.typicode.com/posts'
+
+postForm.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    let title = this.title.value || undefined
+    let body = this.body.value || undefined
+        if(title && body){
+            let postObj = {
+                userId: 100,
+                title,
+                body
+            }
+
+            fetch(URL,{
+                method: 'POST',
+                headers:{
+                    'Content-type': 'Application/JSON'
+                },
+
+                body: JSON.stringify(postObj)
+
+            }) 
+                .then(response => response.json())
+                .then(data=>{
+                    console.log(data)
+                })
+                .catch(e=> console.log(e.message))
+
+        } else{
+            alert('Please Provide Every Details')
+        }
+
+
 })
 
 
-function listItemGenerator(item,no){
-    let li = document.createElement('li')
-    li.className = 'list-group-item'
-    li.innerHTML = `${no}. ${item.title}`
-    return li
-}
+
