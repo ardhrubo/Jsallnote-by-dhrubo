2ce833bc13b104dab9d98bac333db28096bc60bc
ardhrubo <ardhrubo3038@gmail.com>
2024-02-10 21:13:16 +0600

    form validator

--- chapter_eighteen/dom/lesson14.js
+++ chapter_eighteen/dom/lesson14.js
@@ -3,19 +3,38 @@ let form = document.getElementById('form')
 
 
 form.addEventListener('submit',function(e){
+
     e.preventDefault();
-
+    let isvalid = false
     let formData = {};
-
+   
     [...this.elements].forEach(el=>{
         if (el.type !== 'submit'){
-            formData[el.name] = el.value
+            isvalid = validator(el)
+            if(isvalid){
+                formData[el.name] = el.value
+               
+            } else{
+                alert(`${el.name} is required`)
+            }
         }
         
     })
 
-console.log(formData)
+    if(isvalid){
+        console.log(formData)
+        
+        this.reset()
+    }
 
 })
 
+function validator(el){
+    if(!el.value){
+        return false
+    }
+
+
+    return true
+}
 
