26f1caf976d1c99cd7d8139c10d864b7f46383c9
ardhrubo <ardhrubo3038@gmail.com>
2024-02-10 21:42:31 +0600

    added

--- chapter_eighteen/dom/lesson15.js
+++ chapter_eighteen/dom/lesson15.js
@@ -4,8 +4,31 @@ let postList = document.getElementById('posts')
 
 const URL = 'https://jsonplaceholder.typicode.com/posts'
 
-load.addEventListener('on-click',function(){
- console.log('clicked')
+load.addEventListener('click',function(){
+    fetch(URL)
+            .then(response => response.json())
+            .then(posts=>{
+                
+
+                // posts.forEach((post,index) => {
+                //     let listitem = listItemGenerator(post,index+1)
+                //     postList.appendChild(listitem)
+                // })
+
+
+                // let postsList = posts.map((post,index)=> 
+                // listItemGenerator(post,index+1))
+
+                // postList.appendChild(...postsList)
+
+            })
+            .catch(e => console.log(e.message))
 })
 
 
+function listItemGenerator(item,no){
+    let li = document.createElement('li')
+    li.className = 'list-group-item'
+    li.innerHTML = `${no}. ${item.title}`
+    return li
+}
