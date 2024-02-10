6774bdf96e59bacc96b14ea6f29fd40984089be6
ardhrubo <ardhrubo3038@gmail.com>
2024-02-10 20:56:34 +0600

    add

--- chapter_eighteen/dom/lesson14.js
+++ chapter_eighteen/dom/lesson14.js
@@ -1,9 +1,21 @@
 let form = document.getElementById('form')
 
+
+
 form.addEventListener('submit',function(e){
-    e.preventDefault()
-    console.log(e)
-    
+    e.preventDefault();
+
+    let formData = {};
+
+    [...this.elements].forEach(el=>{
+        if (el.type !== 'submit'){
+            formData[el.name] = el.value
+        }
+        
+    })
+
+console.log(formData)
+
 })
 
 
